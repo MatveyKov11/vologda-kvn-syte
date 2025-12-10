dd316fae5e29107c3246adafe2f1a838ac88ba6c
Matvey Kovalev <matvey.kovalev@ispras.ru>
2025-12-11 00:01:43 +0300

    Улучшение таблицы результатов игры

--- frontend/kvn-syte/src/components/ResultTable.vue
+++ frontend/kvn-syte/src/components/ResultTable.vue
@@ -6,15 +6,18 @@
             </div>
         </h2>
     </div>
-    <table class="ui celled table">
-        <thead>
-            <tr><th>Название команды</th>
+    <table class="ui table">
+        <thead><tr>
+            <th>Название команды</th>
             <th v-for="i in columns.length" :key="i">
-                <select v-model="columns[i-1]">
-                    <option disabled value="">Конкурс</option>
-                    <option v-for="contest in contests" :key="contest.id" :value="contest.contestName">{{ contest.contestName }}</option>
-                </select>
-                <i class="minus square icon" @click="deleteColumn(i-1)"></i>
+                <form class="ui form">
+                    <div class="field">
+                        <select class="ui fluid dropdown" v-model="columns[i-1]">
+                            <option disabled value="">Конкурс</option>
+                            <option v-for="contest in contests" :key="contest.id" :value="contest.contestName">{{ contest.contestName }}</option>
+                        </select>
+                    </div>
+                </form>
             </th>
             <th> Всего</th>
             <th><i class="plus icon" @click="addColumn()"></i></th>
@@ -22,16 +25,33 @@
         <tbody>
             <tr v-for="result in results" :key="result.id">
                 <td>
-                    <select v-model="result.team">
-                        <option disabled value="">Команда</option>
-                        <option v-for="team in teams" :key="team.id" :value="team.teamName">{{ team.teamName }}</option>
-                    </select>
+                    <form class="ui form">
+                        <div class="field">
+                            <select class="ui fluid dropdown" v-model="result.team">
+                                <option disabled value="">Команда</option>
+                                <option v-for="team in teams" :key="team.id" :value="team.teamName">{{ team.teamName }}</option>
+                            </select>
+                        </div>
+                    </form>
                 </td>
                 <td v-for="i in result.rates.length" :key="i">
-                    <input class="ui input" type="number" :name="'rate'+i" v-model="result.rates[i-1]">
+                    <div class="ui fluid input">
+                        <input type="number" :name="'rate'+i" v-model="result.rates[i-1]">
+                    </div>
                 </td>
                 <td> {{ sumFinal(result.rates) }}</td>
                 <td><i class="minus square icon" @click="deleteRow(result.id)"></i></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td v-for="i in columns.length" :key="i" align="center">
+                    <div class="ui icon fluid button" @click="deleteColumn(i-1)">
+                        <i class="minus square icon"></i>
+                        Удалить
+                    </div>
+                </td>
+                <td></td>
+                <td></td>
             </tr>
         </tbody>
     </table>
@@ -136,12 +156,12 @@ function addColumn(){
 function deleteColumn(id){
     for(let i = id; i < columns.value.length-1; i++){
         columns.value[i] = columns.value[i+1].slice()
+        for(let j = 0; j < results.value.length; j++){
+            results.value[j].rates[i] = results.value[j].rates[i+1]
+        }
     }
     columns.value.pop()
     for(let i = 0; i < results.value.length; i++){
-        for(let j = id; j < results.value[i].rates.length-1; j++){
-            results.value[i].rates[j] = results.value[i].rates[j+1]
-        }
         results.value[i].rates.pop()
     }
 }
