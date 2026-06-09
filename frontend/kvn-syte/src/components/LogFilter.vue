021420b9436cf4bf6ed76291e4b8d27f1956a8fc
Matvey Kovalev <kovalev_matvey@inbox.ru>
2026-06-09 23:00:21 +0300

    привязка фильтра

--- frontend/kvn-syte/src/components/LogFilter.vue
+++ frontend/kvn-syte/src/components/LogFilter.vue
@@ -8,48 +8,86 @@
         <div class="ui form">
             <div class="four fields">
                 <div class="field">
-                    <label>Кто</label>
+                    <h4 class="ui header">
+                        <div class="content">
+                            Кто
+                        </div>
+                    </h4>
                     <div class="ui checkbox">
-                        <input type="checkbox">
+                        <input type="checkbox" v-model="filter.subject" value="anon">
                         <label>Анонимные пользователи</label>
-                        <input type="checkbox">
+                    </div>
+                    <div class="ui divider"/>
+                    <div class="ui checkbox">
+                        <input type="checkbox" v-model="filter.subject" value="edit">
                         <label>Редакторы</label>
-                        <input type="checkbox">
+                    </div>
+                    <div class="ui divider"/>
+                    <div class="ui checkbox">
+                        <input type="checkbox" v-model="filter.subject" value="adm">
                         <label>Администраторы</label>
                     </div>
                 </div>
                 <div class="field">
-                    <label>Действие</label>
+                    <h4 class="ui header">
+                        <div class="content">
+                            Действие
+                        </div>
+                    </h4>
                     <div class="ui checkbox">
-                        <input type="checkbox">
+                        <input type="checkbox" v-model="filter.action" value="Созд">
                         <label>Создание</label>
-                        <input type="checkbox">
-                        <label>Удаление</label>
-                        <input type="checkbox">
+                    </div>
+                    <div class="ui divider"/>
+                    <div class="ui checkbox">
+                        <input type="checkbox" v-model="filter.action" value="Измен">
                         <label>Изменение</label>
+                    </div>
+                    <div class="ui divider"/>
+                    <div class="ui checkbox">
+                        <input type="checkbox" v-model="filter.action" value="Удал">
+                        <label>Удаление</label>
                     </div>
                 </div>
                 <div class="field">
-                    <label>Объект</label>
+                    <h4 class="ui header">
+                        <div class="content">
+                            Объект
+                        </div>
+                    </h4>
                     <div class="ui checkbox">
-                        <input type="checkbox">
-                        <label>Учетная запись</label>
-                        <input type="checkbox">
-                        <label>Таблица</label>
-                        <input type="checkbox">
+                        <input type="checkbox" v-model="filter.subject" value="post">
                         <label>Пост</label>
-                        <input type="checkbox">
+                    </div>
+                    <div class="ui checkbox">
+                        <input type="checkbox" v-model="filter.subject" value="table">
+                        <label>Таблица</label>
+                    </div>
+                    <div class="ui divider"/>
+                    <div class="ui checkbox">
+                        <input type="checkbox" v-model="filter.action" value="change">
                         <label>Стиль</label>
-                        <input type="checkbox">
+                    </div>
+                    <div class="ui checkbox">
+                        <input type="checkbox" v-model="filter.action" value="user">
+                        <label>Учетная запись</label>
+                    </div>
+                    <div class="ui divider"/>
+                    <div class="ui checkbox">
+                        <input type="checkbox" v-model="filter.action" value="comment">
                         <label>Комментарий</label>
                     </div>
                 </div>
                 <div class="field">
-                    <label>Время</label>
+                    <h4 class="ui header">
+                        <div class="content">
+                            Дата
+                        </div>
+                    </h4>
                     <label>Начало периода</label>
-                    <input type="date">
+                    <input type="datetime-local" v-model="filter.after">
                     <label>Конец периода</label>
-                    <input type="date">
+                    <input type="datetime-local" v-model="filter.before">
                 </div>
             </div>
         </div>
@@ -61,4 +99,17 @@
             <i class="check icon"></i> Применить фильтр
         </button>
     </div>
+    {{ filter }}
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const filter = ref({
+    subject: [],
+    action: [],
+    object: [],
+    before: 0,
+    after: 0
+})
+</script>
