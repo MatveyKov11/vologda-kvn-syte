23e7912d811cf0c4cd52cde432d3fb1dc34239b0
Matvey Kovalev <kovalev_matvey@inbox.ru>
2026-06-19 22:51:14 +0300

    возможность удалять цвет

--- frontend/kvn-syte/src/components/StyleViewer.vue
+++ frontend/kvn-syte/src/components/StyleViewer.vue
@@ -17,36 +17,43 @@
             </div>
         </h4>
         <div class="ui segment" v-for="(col, i) in colors" :key="i">
-                    <h4 class="ui header">
-                        #{{ i+1 }}
-                    </h4>
-                    <table class="ui very basic table">
-                        <tbody><tr>
-                            <td class="one wide">
-                                <div :style="circleStyle(col)"/>
-                            </td>
-                            <td class="left aligned eight wide">
-                                #{{ toHex(col) }}
-                            </td>
-                            <td>
-                                <div class="ui form">
-                                    <div class="field">
-                                        <label for="r">R:</label>
-                                        <input type="number" name="r" v-model="col.r">
-                                    </div>
-                                    <div class="field">
-                                        <label for="g">G:</label>
-                                        <input type="number" name="g" v-model="col.g">
-                                    </div>
-                                    <div class="field">
-                                        <label for="b">B:</label>
-                                        <input type="number" name="b" v-model="col.b">
-                                    </div>
-                                </div>
-                            </td>
-                        </tr></tbody>
-                    </table>
+            <h4 class="ui header">
+                #{{ i+1 }}
+            </h4>
+            <table class="ui very basic table">
+                <tbody><tr>
+                    <td class="one wide">
+                        <div :style="circleStyle(col)"/>
+                    </td>
+                    <td class="left aligned eight wide">
+                        #{{ toHex(col) }}
+                    </td>
+                    <td>
+                        <div class="ui form">
+                            <div class="field">
+                                <label for="r">R:</label>
+                                <input type="number" name="r" v-model="col.r">
+                            </div>
+                            <div class="field">
+                                <label for="g">G:</label>
+                                <input type="number" name="g" v-model="col.g">
+                            </div>
+                            <div class="field">
+                                <label for="b">B:</label>
+                                <input type="number" name="b" v-model="col.b">
+                            </div>
+                        </div>
+                    </td>
+                </tr></tbody>
+            </table>
+            <div class="ui right rail">
+                <div class="ui buttons">
+                    <div class="ui icon red button" @click="removeColor(i)">
+                        <i class="x icon"/>
+                    </div>
                 </div>
+            </div>
+        </div>
         <button class="ui primary button" @click="addColor">
             <i class="plus icon"/> Добавить цвет
         </button>
@@ -100,5 +107,14 @@ function toHex(col){
 
 function addColor(){
     colors.value.push({r:0, g: 255, b: 0})
+}
+function removeColor(r){
+    let colorsCopy = []
+    colors.value.forEach((col, i) => {
+        if(i != r){
+            colorsCopy.push(col)
+        }
+    })
+    colors.value = colorsCopy
 }
 </script>
