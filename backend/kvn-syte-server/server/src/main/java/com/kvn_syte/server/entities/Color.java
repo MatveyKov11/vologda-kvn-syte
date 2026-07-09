31599bea8a9f0880f98971ca49d58e50f58bcfdb
Matvey Kovalev <kovalev_matvey@inbox.ru>
2026-07-09 22:15:41 +0300

    fix color & content

--- backend/kvn-syte-server/server/src/main/java/com/kvn_syte/server/entities/Color.java
+++ backend/kvn-syte-server/server/src/main/java/com/kvn_syte/server/entities/Color.java
@@ -1,7 +1,15 @@
 package com.kvn_syte.server.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Color {
-    Long colorId;
+    Long id;
+    Long styleId;
 
     Long r;
     Long g;
--- backend/kvn-syte-server/server/src/main/java/com/kvn_syte/server/entities/Content.java
+++ backend/kvn-syte-server/server/src/main/java/com/kvn_syte/server/entities/Content.java
@@ -1,5 +1,16 @@
 package com.kvn_syte.server.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Content {
+    Long id;
+    Long blockId;
+
+    Long position;
     String data;
 }
