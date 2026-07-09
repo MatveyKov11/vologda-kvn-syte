befceb0edd740f4c8c0ef0ab1d79a7051edc2902
Matvey Kovalev <kovalev_matvey@inbox.ru>
2026-07-09 22:23:07 +0300

    добавление класса таблицы

---
+++ backend/kvn-syte-server/server/src/main/java/com/kvn_syte/server/entities/Table.java
@@ -0,0 +1,18 @@
+package com.kvn_syte.server.entities;
+
+import java.util.ArrayList;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Table {
+    Long id;
+
+    String title;
+    ArrayList<Column> columns;
+    ArrayList<ArrayList<Cell>> cells;
+}
