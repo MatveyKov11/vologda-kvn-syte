fb54a3ecd6256b88b84aaab535aca7d28afe6862
Matvey Kovalev <kovalev_matvey@inbox.ru>
2026-07-10 23:04:29 +0300

    добавление тестовых данных-логов

--- backend/kvn-syte-server/server/src/main/java/com/kvn_syte/server/entities/Log.java
+++ backend/kvn-syte-server/server/src/main/java/com/kvn_syte/server/entities/Log.java
@@ -2,7 +2,9 @@ package com.kvn_syte.server.entities;
 
 import com.kvn_syte.server.entities.Log.ActionType;
 
+import java.util.ArrayList;
 import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -75,5 +77,33 @@ public class Log {
                     return "???";
             }
         }
+    }
+
+    public ArrayList<Log> testData(){
+        ArrayList<Log> a = new ArrayList<>();
+        a.add(new Log( 0L, 1L, UserType.Admin, ActionType.Add, 4L, ObjectType.User, new Date()));
+        a.add(new Log( 1L, 1L, UserType.Admin, ActionType.Block, 4L, ObjectType.User, new Date()));
+        a.add(new Log( 2L, 1L, UserType.Admin, ActionType.Delete, 4L, ObjectType.User, new Date()));
+        a.add(new Log( 3L, 1L, UserType.Admin, ActionType.Add, 2L, ObjectType.User, new Date()));
+
+        a.add(new Log( 4L, 2L, UserType.Admin, ActionType.Add, 1L, ObjectType.Table, new Date()));
+        a.add(new Log( 5L, 1L, UserType.Admin, ActionType.Add, 2L, ObjectType.Table, new Date()));
+        a.add(new Log( 6L, 2L, UserType.Admin, ActionType.Change, 2L, ObjectType.Table, new Date()));
+        a.add(new Log( 7L, 2L, UserType.Admin, ActionType.Delete, 2L, ObjectType.Table, new Date()));
+
+        a.add(new Log( 8L, 1L, UserType.Admin, ActionType.Add, 1L, ObjectType.Post, new Date()));
+        a.add(new Log( 9L, 2L, UserType.Admin, ActionType.Add, 2L, ObjectType.Post, new Date()));
+        a.add(new Log(10L, 1L, UserType.Admin, ActionType.Change, 2L, ObjectType.Post, new Date()));
+        a.add(new Log(11L, 1L, UserType.Admin, ActionType.Add, 2L, ObjectType.Post, new Date()));
+
+        a.add(new Log(12L, -1L, UserType.Anon, ActionType.Add, 1L, ObjectType.Comment, new Date()));
+        a.add(new Log(13L, -1L, UserType.Anon, ActionType.Add, 2L, ObjectType.Comment, new Date()));
+        a.add(new Log(14L, 2L, UserType.Admin, ActionType.Delete, 1L, ObjectType.Comment, new Date()));
+
+        a.add(new Log(15L, 1L, UserType.Admin, ActionType.Add, 1L, ObjectType.Style, new Date()));
+        a.add(new Log(16L, 1L, UserType.Admin, ActionType.Add, 2L, ObjectType.Style, new Date()));
+        a.add(new Log(17L, 2L, UserType.Admin, ActionType.Change, 2L, ObjectType.Style, new Date()));
+        a.add(new Log(18L, 2L, UserType.Admin, ActionType.Delete, 2L, ObjectType.Style, new Date()));
+        return a;
     }
 }
--- backend/kvn-syte-server/server/src/main/java/com/kvn_syte/server/repos/LogRepository.java
+++ backend/kvn-syte-server/server/src/main/java/com/kvn_syte/server/repos/LogRepository.java
@@ -1,22 +1,15 @@
 package com.kvn_syte.server.repos;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 import org.springframework.stereotype.Repository;
 
 import com.kvn_syte.server.entities.Log;
-import com.kvn_syte.server.entities.Log.UserType;
-import com.kvn_syte.server.entities.Log.ActionType;
-import com.kvn_syte.server.entities.Log.ObjectType;
 
 @Repository
 public class LogRepository {
     public ArrayList<Log> getAll(){
-        ArrayList<Log> a = new ArrayList<>();
-        a.add(new Log(0L, 0L, UserType.Anon, ActionType.Add, 0L, ObjectType.Post, new Date()));
-        a.add(new Log(1L, 0L, UserType.Anon, ActionType.Add, 0L, ObjectType.Post, new Date()));
-        a.add(new Log(2L, 0L, UserType.Anon, ActionType.Add, 0L, ObjectType.Post, new Date()));
-        return a;
+        Log a = new Log();
+        return a.testData();
     }
 }
